b6e8cc562d71a18878a60008cafd7838bed7dc20
Virginia <vihvasc@gmail.com>
2021-07-28 20:58:07 -0300

    Adiciona primeiros passos parte 2

--- exerc-bl-4/scripts.js
+++ exerc-bl-4/scripts.js
@@ -1,8 +1,16 @@
-const myName = "Virgínia";
+/* const myName = "Virgínia";
 const birthCity = "Belo Horizonte";
 let birthYear = 1994;
 birthYear = 2030;
 
 console.log(myName);
 console.log(birthCity);
-console.log(birthYear);
+console.log(birthYear); */
+
+let base = 5;
+let altura = 8;
+let area = base*altura;
+let perimetro = (base+altura)*2;
+
+console.log(area);
+console.log(perimetro);
