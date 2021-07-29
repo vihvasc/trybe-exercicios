39be25f4a54186949b94aa6df4099d4a1c545e76
Virginia <vihvasc@gmail.com>
2021-07-28 21:36:05 -0300

    Exercicio 1

---
+++ exerc-bl-4/pratica-4.1/scripts.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 9;
+
+let soma = a + b;
+console.log(soma);
+
+let subtrai = a - b;
+console.log(subtrai);
+
+let multiplica = a * b;
+console.log(multiplica);
+
+let divide = a / b;
+console.log(divide);
+
+let modulo = a % b;
+console.log(modulo);
