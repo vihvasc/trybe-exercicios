80218da5fbd758939f2b54d6af60deb8b432698c
Virginia <vihvasc@gmail.com>
2021-07-28 21:21:54 -0300

    Adiciona primeiros passos parte 4

--- exerc-bl-4/scripts.js
+++ exerc-bl-4/scripts.js
@@ -15,7 +15,7 @@ let perimetro = (base+altura)*2;
 console.log(area);
 console.log(perimetro); */
 
-let notaCandidato = 75;
+/* let notaCandidato = 75;
 
 if (notaCandidato >= 80) {
     console.log("Parabéns, você foi aprovada(o)!")
@@ -25,4 +25,23 @@ else if (notaCandidato < 80 && notaCandidato >= 60) {
 }
 else  {
     console.log("Você foi reprovada(o)")
+} */
+
+let candidata = "aprovada";
+
+switch (candidata) {
+    case "aprovada":
+     console.log("Você foi aprovada!");
+     break;
+
+    case "reprovada":
+     console.log("Você foi reprovada!");
+     break;
+
+    case "lista":
+     console.log("Você está na lista de espera!");
+     break;
+
+    default:
+     console.log("Não se aplica")
 }
