5e92cb7c77377e143cdffc73a5dcd446f3fea70e
Virginia <vihvasc@gmail.com>
2021-07-28 21:51:07 -0300

    Exercicio 3

--- exerc-bl-4/pratica-4.1/scripts.js
+++ exerc-bl-4/pratica-4.1/scripts.js
@@ -20,7 +20,7 @@ console.log(modulo); */
 
 /* Exercício 2 */
 
-let a = 5;
+/* let a = 5;
 let b = 9;
 
 if (a > b) {
@@ -28,4 +28,20 @@ if (a > b) {
 }
 else {
     console.log("b é maior que a")
+} */
+
+/* Exercício 3 */
+
+let a = 5;
+let b = 9;
+let c = 3;
+
+if (a > b && a > c) {
+    console.log("a é maior que b e c")
+}
+else if (a < b && c < b) {
+    console.log("b é maior que a e c")
+}
+else {
+    console.log("c é maior que a e b")
 }
