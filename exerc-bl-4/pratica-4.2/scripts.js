112d05f8deada642164a2add61c38bee55021da9
Virginia <vihvasc@gmail.com>
2021-07-31 18:48:31 -0300

    Exercicio 4

--- exerc-bl-4/pratica-4.2/scripts.js
+++ exerc-bl-4/pratica-4.2/scripts.js
@@ -18,6 +18,18 @@ console.log(soma); */
 
 //Exercício 3 - calcule e imprima a média aritmética dos valores contidos no array
 
+/* let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    soma = soma + numbers[index];
+}
+
+console.log(numbers.length);
+console.log(soma);
+console.log(soma / numbers.length) */
+
+//Exercício 4 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20"
+
 let soma = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
@@ -27,3 +39,10 @@ for (let index = 0; index < numbers.length; index += 1) {
 console.log(numbers.length);
 console.log(soma);
 console.log(soma / numbers.length)
+
+if (soma / numbers.length > 20) {
+    console.log("valor maior que 20")
+} 
+else {
+    console.log("valor menor que 20")
+}
