9c0d5ce8fbcb9f9a7130d76f42e2e90aaa71e0b7
Virginia <vihvasc@gmail.com>
2021-07-31 18:21:25 -0300

    Exercicio 2

--- exerc-bl-4/pratica-4.2/scripts.js
+++ exerc-bl-4/pratica-4.2/scripts.js
@@ -1,11 +1,17 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-//exercício 1
+//exercício 1 - percorra o array imprimindo todos os valores nele contidos com a função console.log() 
+
+/* for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);
+} */
+
+//Exercício 2 - some todos os valores contidos no array e imprima o resultado
+
+let soma = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
+    soma = soma + numbers[index];
 }
 
-
-//Exercício 2
-
+console.log(soma);
