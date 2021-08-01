a41f054d87f949b3f81c5a4dda0dea0657b8f407
Virginia <vihvasc@gmail.com>
2021-07-31 21:06:25 -0300

    Exercicio 3

--- exerc-bl-4/pratica-4.3/scripts.js
+++ exerc-bl-4/pratica-4.3/scripts.js
@@ -1,24 +1,41 @@
 //Exercício 1 - faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
 
 let n = 5;
-let recebe = "";
+let string = "";
 let asterisco = "*";
+let espaco = " "
 
-for (let index = 0; index < n; index += 1) {
-    recebe += asterisco;
+/* for (let index = 0; index < n; index += 1) {
+    string += asterisco;
 }
 
 for (let index = 0; index < n; index += 1){
-    console.log(recebe);
-}
-
-
-
+    console.log(string);
+} */
 
 //Exercício 2 - faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base
 
 /* for (let index = 0; index < n; index += 1) {
-    recebe += asterisco;
-    console.log(recebe);
+    string += asterisco;
+    console.log(string);
 } */
+
+//Exercício 3 - Agora inverta o lado do triângulo
+
+//espaco = n - index
+//asterisco = index
+
+for (let index = 1; index <= n; index += 1) {
+    string = "";
+
+    for (let espacoIndex = 0; espacoIndex < n - index; espacoIndex += 1){
+        string += espaco;
+    }
+
+    for (let asteriscoIndex = 0; asteriscoIndex < index; asteriscoIndex +=1) {
+        string += asterisco;
+    }
+    
+    console.log(string);
+}
 
