589f26c1120e75dbf7075e8837acf7f47cb51f69
Virginia <vihvasc@gmail.com>
2021-08-09 21:34:44 -0300

    Exercicio 2

--- exerc-bl-5/bl-5.1/scripts.js
+++ exerc-bl-5/bl-5.1/scripts.js
@@ -3,7 +3,7 @@
         - document.getElementById()
         - document.getElementsByClassName()
         - document.getElementsByTagName()
- Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+
  Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
  Crie uma função que mude a cor do quadrado vermelho para branco.
  Crie uma função que corrija o texto da tag <h1>.
@@ -11,7 +11,12 @@
  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
         */
 
- function pTagColor() {
+ function pTagText() {
     document.querySelector("p").innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
  }
- pTagColor();
+ pTagText();
+
+function squareColor() {
+    document.querySelector(".main-content").style.backgroundColor = "rgb(76, 164, 109)";
+}
+squareColor();
