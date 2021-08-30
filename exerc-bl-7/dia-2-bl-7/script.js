98182dfbb3b40a998dce0c174a96809122b78d74
Virginia <vihvasc@gmail.com>
2021-08-30 14:08:31 -0300

    Fixaçao 2

--- exerc-bl-7/dia-2-bl-7/script.js
+++ exerc-bl-7/dia-2-bl-7/script.js
@@ -1,33 +1,27 @@
-const customer1 = {
-    firstName: 'Roberto',
-    age: 22,
-    job: 'Teacher',
+const student1 = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkills: 'Ótimo',
 };
 
-console.log(Object.entries(customer1));
-
-customer1.lastname = 'Faria';
-customer1['country'] = 'BRASIL';
-
-console.log(customer1);
-
-const customer = {
-    firstName: 'Roberto',
-    age: 22,
-    job: 'Teacher',
+const student2 = {
+    Html: 'Bom',
+    Css: 'Ótimo',
+    JavaScript: 'Ruim',
+    SoftSkills: 'Ótimo',
+    Git: 'Bom', // chave adicionada
 };
 
-let newKey = 'lastName';
-const lastName = 'Ferreira';
-customer[newKey] = lastName;
-newKey = 'fullName';
-const fullName = `${customer.firstName} ${customer.lastName}`;
-customer[newKey] = fullName;
-console.log(customer);
+const listSkills = (student) => {
+    const arrayOfSkills = Object.keys(student);
+    for(index in arrayOfSkills){
+      console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+    }
+};
 
-const makeObject = (object, key, value) => {
-    object[key] = value;
-    return object
-}
+console.log('Estudante 1');
+listSkills(student1);
 
-console.log(makeObject({}, 'name', 'Roberto'));
+console.log('Estudante 2');
+listSkills(student2); 
