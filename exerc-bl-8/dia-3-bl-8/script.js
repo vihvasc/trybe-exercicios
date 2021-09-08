8cea06b5edef82976fc702130daf3aca9fc0225b
Virginia <vihvasc@gmail.com>
2021-09-08 11:47:18 -0300

    Fixacao 2

--- exerc-bl-8/dia-3-bl-8/script.js
+++ exerc-bl-8/dia-3-bl-8/script.js
@@ -1,36 +1,23 @@
-const numbers = [19, 21, 30, 3, 45, 22, 15];
-
-const verifyEven = (number) => number % 2 === 0;
-
-const isEven = numbers.filter(verifyEven);
-
-console.log(isEven); // [ 30, 22 ]
-
-// Outra forma de ser realizada sem a necessidade de criar uma nova função.
-const isEven2 = numbers.filter((number) => number % 2 === 0);
-
-console.log(isEven2); // [ 30, 22 ]
-
-const objPeople = [
-    { name: 'José', age: 21 },
-    { name: 'Lucas', age: 19 },
-    { name: 'Maria', age: 16 },
-    { name: 'Gilberto', age: 18 },
-    { name: 'Vitor', age: 15 },
+const persons = [
+    { firstName: 'Maria', lastName: 'Ferreira' },
+    { firstName: 'João', lastName: 'Silva' },
+    { firstName: 'Antonio', lastName: 'Cabral' },
   ];
   
-const verifyAgeDrive = (arrayOfPeople) => (
-    arrayOfPeople.filter((people) => (people.age < 18))
-  );
+  const fullNames = [];
   
-console.log(verifyAgeDrive(objPeople));
-// [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
+  for (let index = 0; index < persons.length; index += 1) {
+    fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+  }
+  
+  console.log(fullNames);
 
-const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
-
-const removeStudentByName = (name, listStudents) =>
-listStudents.filter((student) => student !== name);
-// Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
-
-const newListStudents = removeStudentByName('Ricardo', arrayMyStudents);
-console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
+const persons = [
+    { firstName: 'Maria', lastName: 'Ferreira' },
+    { firstName: 'João', lastName: 'Silva' },
+    { firstName: 'Antonio', lastName: 'Cabral' },
+  ];
+  
+const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+  
+  console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
